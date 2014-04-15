888d070e7c1de0328cacb007efe14628503c18b7
Rogério Jun Nakatani <rogerjnak@gmail.com>
2014-04-15 09:26:38 -0300

    second change

--- FragmentStart/app/src/main/java/com/example/fragmentstart/app/MainActivity.java
+++ FragmentStart/app/src/main/java/com/example/fragmentstart/app/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.fragmentstart.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements FragmentA.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +13,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        System.out.println("onFragmentInteraction() " + uri);
+    }
 
 }
